cff35fb65b53801230e75a7e0729954717536457
Cakirgenc Alper <60131@etu.he2b.be>
2025-05-26 20:28:38 +0200

    initial commit for the backend

---
+++ memory-backend/routes/game.js
@@ -0,0 +1,13 @@
+// routes/game.js
+const express = require("express");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  res.json("Get Request !");
+});
+
+router.post("/", async (req, res) => {
+  res.status(200).json("Post Request !");
+});
+
+module.exports = router;
