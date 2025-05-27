71e2a51b88de90a484afd089496c165dc53bca40
Hercule-26 <157634078+Hercule-26@users.noreply.github.com>
2025-05-27 20:13:57 +0200

    Adjustint authentification on the frontend

--- memory-frontend/src/views/HomeView.vue
+++ memory-frontend/src/views/HomeView.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
+import { RouterLink } from 'vue-router'
+
 </script>
 
 <template>
   <main>
-    HomeView
+    <nav>
+      <RouterLink to="/game/create">Create game</RouterLink>
+      <RouterLink to="/game/create">Join game</RouterLink>
+    </nav>
   </main>
 </template>
