017c465442db2b9342747778d2f7a4cf032e79f2
Hercule-26 <157634078+Hercule-26@users.noreply.github.com>
2025-05-27 17:23:10 +0200

    added web socket to the backend

--- memory-backend/server.js
+++ memory-backend/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+const http = require("http");
+
 require("dotenv").config();
 
-const { connectToDB } = require("./repository/db");
 const gameRoutes = require("./routes/gameRoute");
 
 const app = express();
@@ -13,10 +14,35 @@ app.use(express.json());
 
 app.use("/game", gameRoutes);
 
-app.listen(PORT, () => {
+// Creating a http server with express
+const server = http.createServer(app);
+
+const WebSocket = require("ws");
+
+// Creation of a WebSocket linked to http server
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (ws) => {
+  console.log("Client connected");
+
+  ws.on("message", (message) => {
+    const data = JSON.parse(message);
+    if (data.type === "createGame") {
+      ws.send(`Game '${data.gameName}' created successfully!`);
+    }
+  });
+  
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+
+// Listen with http
+server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
 
+// const { connectToDB } = require("./repository/db");
 // connectToDB()
 //   .then(() => {
 //     app.listen(PORT, () => {
