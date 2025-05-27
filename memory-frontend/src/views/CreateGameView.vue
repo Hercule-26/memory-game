6400ec57bfe30ef84e285d28500bed9c19448743
Hercule-26 <157634078+Hercule-26@users.noreply.github.com>
2025-05-27 17:07:26 +0200

    Doing game creation page

---
+++ memory-frontend/src/views/CreateGameView.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted } from "vue"
+
+const gameName = ref("");
+const errorMessage = ref<tring | null>(null);
+
+let socket: WebSocket | null = null;
+
+onMounted(() => {
+  socket = new WebSocket("ws://localhost:3000");
+
+  socket.onopen = () => {
+    console.log("WebSocket connected");
+  };
+
+  socket.onmessage = (event: MessageEvent) => {
+    console.log("Server message:", event.data);
+  };
+
+  socket.onerror = (event: Event) => {
+    console.error("WebSocket error:", event);
+  };
+
+  socket.onclose = () => {
+    console.log("🔌 WebSocket closed");
+  };
+});
+
+onUnmounted(() => {
+  if (socket) {
+    socket.close();
+  }
+});
+
+function createGame(): void {
+  if (gameName.value === "") {
+    errorMessage.value = "The name of the game must not be blank";
+  } else if (gameName.value.includes(" ")) {
+    errorMessage.value = "The name of the game must not contain spaces";
+  } else {
+    errorMessage.value = null;
+    console.log(gameName.value);
+
+    if (socket && socket.readyState === WebSocket.OPEN) {
+      const payload = {
+        type: "createGame",
+        gameName: gameName.value,
+      };
+
+      socket.send(JSON.stringify(payload));
+      console.log("Sent to server:", payload);
+    } else {
+      console.error("WebSocket not connected");
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h2>Create a Game :</h2>
+    <form @submit.prevent="createGame">
+      <input type="text" v-model="gameName" required />
+      <button type="submit">Create game</button>
+    </form>
+    <span v-if="errorMessage" style="color: red">{{ errorMessage }}</span>
+  </div>
+</template>
