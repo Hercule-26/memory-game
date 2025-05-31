2a6ae96def8588f0c9c53c0bbb4302540dad396f
Alper Cakirgenc <60131@etu.he2b.be>
2025-06-01 00:40:05 +0200

    cleaning the code + removing logs

--- memory-backend/server.js
+++ memory-backend/server.js
@@ -24,7 +24,7 @@ app.use(
     saveUninitialized: false,
     store,
     cookie: {
-      maxAge: 1000 * 60 * 60, // 1 hour,
+      maxAge: 1000 * 60 * 5, // 5 min,
       secure: false,
       httpOnly: true,
     },
