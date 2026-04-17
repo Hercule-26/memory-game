1d202c8a0c99c3f71a0555c9b552b750da4fd1d3
Cakirgenc Alper <cakirgenc64@gmail.com>
2026-04-17 21:48:14 +0200

    Adding google adsense

---
+++ memory-frontend/src/components/AdBanner.vue
@@ -0,0 +1,21 @@
+<template>
+  <ins class="adsbygoogle"
+       style="display:block"
+       data-ad-client="ca-pub-8743731971392703"
+       data-ad-slot="1234567890"
+       data-ad-format="auto"
+       data-full-width-responsive="true"></ins>
+</template>
+
+<script>
+export default {
+  name: "AdBanner",
+  mounted() {
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (e) {
+      console.error(e);
+    }
+  }
+};
+</script>
--- memory-frontend/src/views/HomeView.vue
+++ memory-frontend/src/views/HomeView.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
   import { RouterLink } from 'vue-router'
+  import AdBanner from '../components/AdBanner.vue'
+
 </script>
 
 <template>
+  <AdBanner />
   <nav class="flex flex-col gap-6">
     <RouterLink to="/game/create"
       class="block px-8 py-3 bg-violet-500 text-white rounded shadow-md hover:bg-violet-600 focus:outline-none focus:ring-2 focus:ring-violet-400 transition text-center font-medium">
